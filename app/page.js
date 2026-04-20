1b7d3790556fe11460d946b561f4fd4fa099454a
hayyantahirr <hayyantahir7@gmail.com>
2026-04-20 17:12:00 +0500

    printing section added

--- app/page.js
+++ app/page.js
@@ -4,18 +4,21 @@ import Sec1 from "@/component/home/Sec1";
 import Sec2 from "@/component/home/Sec2";
 import Sec3 from "@/component/home/Sec3";
 import Sec4 from "@/component/home/Sec4";
+import Sec5 from "@/component/home/Sec5";
 import React from "react";
 
 const page = () => {
   return (
     <>
       <Hero />
-      {/* Our mission */}
-      <Sec1 />
+      {/* Our printing  */}
+      <Sec5/>
       {/* Our Process */}
       <Sec2 />
       {/* Our Certifications */}
       <Sec3/>
+      {/* Our Top Clients */}
+      <Sec1 />
       {/* Team */}
       <Sec4/>
       {/* FAQs */}
